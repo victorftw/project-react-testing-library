465290a439e2fd2abca84672888da8d64c9023e7
Victor Hugo <victor.hiraiftw@outlook.com>
2022-09-19 16:11:11 -0300

    requisito 2 concluido

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import About from '../pages/About';
+import renderWithRouter from '../utils/renderWithRouter';
+
+describe('Testes do componente About', () => {
+  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex;', () => {
+    renderWithRouter(<About />);
+    const paragraphOne = screen.getByText(/this application simulates a pokédex/i);
+    expect(paragraphOne).toBeInTheDocument();
+    const paragraphTwo = screen.getByText(/one can filter pokémons by type, and see/i);
+    expect(paragraphTwo).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+    const title = screen.getByRole('heading', { name: /about pokédex/i, level: 2 });
+    expect(title).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
+    renderWithRouter(<About />);
+    const IMAGE_URL = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const image = screen.getByRole('img', { name: /pokédex/i });
+    expect(image.src).toBe(IMAGE_URL);
+  });
+});
