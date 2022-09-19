42fc41a28eb9d8eee52ab062d3dcc9afe9c89ca7
Victor Hugo <victor.hiraiftw@outlook.com>
2022-09-19 16:49:41 -0300

    requisito 3 concluido

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import FavoritePokemons from '../pages/FavoritePokemons';
+import renderWithRouter from '../utils/renderWithRouter';
+
+const FAVORITE_POKEMON = [{
+  id: 25,
+  name: 'Pikachu',
+  type: 'Electric',
+  averageWeight: {
+    value: '6.0',
+    measurementUnit: 'kg',
+  },
+  image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+}];
+
+describe('Testes do componente FavoritePokemons', () => {
+  test('Teste se é exibida na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const text = screen.getByText(/no favorite pokemon found/i);
+    expect(text).toBeInTheDocument();
+  });
+
+  test('Teste se são exibidos todos os cards de pokémons favoritados', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ FAVORITE_POKEMON } />);
+    const fav = screen.getByRole('img', { name: /pikachu is marked as favorite/i });
+    expect(fav).toBeInTheDocument();
+  });
+});
